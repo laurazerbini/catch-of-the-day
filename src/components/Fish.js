682f635d156efa1d5576329bf6fce11c80821c50
laura zerbini <lauraczerbini@gmail.com>
2021-11-25 16:57:14 +0100

    react

--- src/components/Fish.js
+++ src/components/Fish.js
@@ -1,9 +1,20 @@
 import React from 'react';
 // eslint-disable-next-line
 import AddFishForm from './AddFishForm';
+import PropTypes from 'prop-types';
 import {formatPrice} from '../helpers';
 
 class Fish extends React.Component {
+  static PropTypes = {
+    details: PropTypes.shape({
+      image: PropTypes.string,
+      name: PropTypes.string,
+      desc: PropTypes.string,
+      status: PropTypes.string,
+      price: PropTypes.number,
+    }),
+    addToOrder: PropTypes.func,
+  }
     handleClick = () => {
       this.props.addToOrder(this.props.index);
     }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,7 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
+// this is a stateless functional component
 const Header = (props) => (
     <header className="top">
     <h1>
@@ -14,26 +16,10 @@ const Header = (props) => (
         <span>{props.tagline}</span>
     </h3>
     </header>
-  );
+);
 
-// class Header extends React.Component {
-//   render() {
-//       return (
-//         <header className="top">
-//         <h1>
-//           Catch
-//           <span className="ofThe">
-//             <span className="of">Of</span>
-//             <span className="the">The</span>
-//           </span>
-//           Day
-//         </h1>
-//         <h3 className="tagline">
-//             <span>{this.props.tagline}</span>
-//         </h3>
-//         </header>
-//       )
-//   }
-// }
+Header.PropTypes = {
+ tagline: PropTypes.string.isRequired
+}
 
 export default Header;
